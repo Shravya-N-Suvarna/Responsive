c457430852d7c64bc805081ac93281de5811b8cf
Shravya <shravyasuvarna1998@gmail.com>
2024-06-11 12:38:25 +0530

    Changes made

--- responsive/src/App.js
+++ responsive/src/App.js
@@ -1,12 +1,12 @@
-import Count from './Count';
-
+// import Count from './Count';
+import Counter from './Counter';
 import './App.css';
 
 function App() {
  
   return (
     <div className="App">
-      <Count/>
+      <Counter/>
       {/* <div className='bg-orange-500 w-full h-56 sm:bg-red-400 md:bg-green-500 lg:bg-pink-600 xl:bg-blue-500'>
         <h1 className='text-white'>Accelerlab Solutions</h1>
         <div className='flex flex-col md:flex-row bg-amber-200'>
--- responsive/src/Counter.js
+++ responsive/src/Counter.js
@@ -4,9 +4,12 @@ export default function Counter() {
     const[count,setCount]=useState(0)
 
   return (
-    <div><p>{count}</p>
-    <button onClick={()=>setCount(count+1)}>Increase</button>
-    <button onClick={()=>setCount(count-1)}>Decrease</button>
+    <div className='h-[100vh] w-full flex justify-center items-center'>
+      <div>
+        <h1 className='text-[40px] text-center mb-4 font-bold'>{count}</h1>
+    <button className="p-2 border border-black my-2 bg-red-500 hover:bg-red-400 text-white font-bold mx-3" onClick={()=>setCount(count+1)}>Increase</button>
+    <button className="p-2 border border-black my-2 bg-red-500 hover:bg-red-400 text-white font-bold mx-3"onClick={()=>setCount(count-1)}>Decrease</button>
+    </div>
     </div>
   )
 }
