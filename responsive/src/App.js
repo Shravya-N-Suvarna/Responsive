3dfc155e10baf4040e7782763248c205d4cfae2a
Shravya <shravyasuvarna1998@gmail.com>
2024-06-11 10:56:42 +0530

    first commit

---
+++ responsive/src/App.js
@@ -0,0 +1,23 @@
+import Count from './Count';
+
+import './App.css';
+
+function App() {
+ 
+  return (
+    <div className="App">
+      <Count/>
+      {/* <div className='bg-orange-500 w-full h-56 sm:bg-red-400 md:bg-green-500 lg:bg-pink-600 xl:bg-blue-500'>
+        <h1 className='text-white'>Accelerlab Solutions</h1>
+        <div className='flex flex-col md:flex-row bg-amber-200'>
+        {/* <div className='flex flex-row bg-amber-200'> */}
+          {/* <div className='text-black flex md:flex-row w-full justify-center items-center'>Moodbidri</div>
+          <div className='text-black flex flex-col w-full justify-center items-center'>Mangalore</div>
+        </div>
+      </div> */}
+       {/* */} 
+    </div>
+  );
+}
+
+export default App;
---
+++ responsive/src/Count.js
@@ -0,0 +1,12 @@
+import React, { useState } from 'react'
+
+export default function Count() {
+    const[count,setCount]=useState(0)
+  return (
+    <div>
+        <h1>{count}</h1>
+        <button onClick={()=>setCount(count+1)}>Increase</button>
+        <button onClick={()=>setCount(count-1)}>Decrease</button>
+        </div>
+  )
+}
