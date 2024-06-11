5e32720f5e18c7acc36ff0a4c66db798e62f744f
Shravya <shravyasuvarna1998@gmail.com>
2024-06-11 12:25:52 +0530

    Changes made

--- responsive/src/Count.js
+++ responsive/src/Count.js
@@ -5,8 +5,13 @@ export default function Count() {
   return (
     <div>
         <h1>{count}</h1>
-        <button onClick={()=>setCount(count+1)}>Increase</button>
-        <button onClick={()=>setCount(count-1)}>Decrease</button>
+        <div className='bg-orange-500 w-full h-40'>
+            <div className='flex flex-row w-full justify-center items-center'>
+            <div className='bg-green-500 flex flex-col w-full justify-center items-center 	border-style: solid'>
+        <button onClick={()=>setCount(count+1)}>Increase</button></div>
+        <div className='bg-blue-500 flex flex-col w-full justify-center items-center 	border-style: solid'><button onClick={()=>setCount(count-1)}>Decrease</button></div>
+        </div>
+        </div>
         </div>
   )
 }
